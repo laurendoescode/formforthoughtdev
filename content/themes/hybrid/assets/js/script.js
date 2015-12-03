4e895f128e5a1f3a8abae38654d3ee19b41d7c80
Lauren Kelly <hi@laurenkelly.me>
2015-12-03 14:48:37 +0100

    update to js for slidebox cta

--- content/themes/hybrid/assets/js/script.js
+++ content/themes/hybrid/assets/js/script.js
@@ -1,6 +1,7 @@
 /*  TABLE OF CONTENT
     1. Common function
     2. Initialing
+    3. CTA side popup
 */
 /*================================================================*/
 /*  1. Common function
@@ -817,3 +818,26 @@ paceOptions = {
 	    sfApp.resizeEvents();    	    
 	});
 })(jQuery);
+
+/*================================================================*/
+/*  3. CTA slidebox popup
+/*================================================================*/
+
+    $(function() {
+      var slidebox = $('#slidebox');
+      if (slidebox.length>0) {
+        $(window).scroll(function(){
+            var distanceTop = $('#last').offset().top - $(window).height();
+            if  ($(window).scrollTop() > distanceTop){
+                slidebox.animate({'right':'0px'},300);
+            }
+            else{
+                slidebox.stop(true).animate({'right':'-430px'},100);
+            }
+        });
+        $('#slidebox .close').on('click',function(){
+            $(this).parent().remove();
+        });
+      }
+    });
+
